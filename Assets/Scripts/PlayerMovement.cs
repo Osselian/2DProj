5c4a0b7e2449dd947e1a91e27e931de8812fd1d2
Osselian <rossemir11@gmail.com>
2021-07-02 17:53:36 +0300

    Comments_004

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private bool _grounded;
 
     private Rigidbody2D _rigidbody2D;
-    private Vector2 _velocity;
+    private Vector2 _targetVelocity;
     private RaycastHit2D[] _hitBuffer = new RaycastHit2D[16];
     private List<RaycastHit2D> _hitBufferList = new List<RaycastHit2D>(16);    
     private float _minimumJumpDistance = 0.25f;
@@ -24,14 +24,13 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
-        _velocity = _rigidbody2D.velocity;    
     }
 
     private void Update()
     {
         _grounded = false;
         DeltaPosition = Vector2.zero;
-        
+
         int count = _rigidbody2D.Cast(Vector2.down, _hitBuffer, _minimumJumpDistance);
 
         _hitBufferList.Clear();
@@ -50,16 +49,11 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            DeltaPosition = Vector2.left * _speed * Time.deltaTime;
-            _rigidbody2D.position = _rigidbody2D.position + DeltaPosition;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            DeltaPosition = Vector2.right * _speed * Time.deltaTime;
-            _rigidbody2D.position = _rigidbody2D.position + DeltaPosition;
-        }
+        _targetVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+
+        DeltaPosition = _targetVelocity * _speed * Time.deltaTime;
+        _rigidbody2D.position = _rigidbody2D.position + DeltaPosition;
+
         if (Input.GetKey(KeyCode.Space) && _grounded)
         {
             _rigidbody2D.AddForce(Vector2.up * _jumpForce);
